08cb8b3a14f9cc3b84bc4186541dfeddf72012af
j40653263 <huseyinkarakaya@std.iyte.edu.tr>
2020-10-19 09:56:01 +0000

    LAB1/Examples

--- lab1/example1.py
+++ lab1/example1.py
@@ -1,1 +1,54 @@
-print("Hello World!")
+#Example1
+""""
+total_animals = 10
+chickens = 6 
+roosters = total_animals - chickens
+print(roosters) 
+"""
+
+#Example2
+""""
+x = 1
+y = 4
+z = 0.25
+
+res = (((2*x+y)**2)*(z**0.5)) / ((x**0.5) + (y**0.5))
+print(res)
+"""
+
+#Example3
+"""
+a = 2
+b = 6
+c = -20
+
+x1 = (b + (b**2 - 4*a*c)**0.5) / (2*a)
+x2 = (b - (b**2 - 4*a*c)**0.5) / (2*a)
+print(x1)
+print(x2)
+"""
+
+#Example4
+"""
+celcius = float(input("What is the temprature: "))
+fahr = celcius * 1.8 + 32
+print(fahr)
+"""
+
+#Example5
+"""
+edge1 = float(input("Enter the lenght of first edge: "))
+edge2 = float(input("Enter the lenght of second edge: "))
+hipo = (edge1**2 + edge2**2)**0.5
+print(hipo)
+"""
+
+#Example6
+
+v1 = 80
+v2 = 70 
+distance = 490
+f_distance = 150
+hour = (distance - f_distance) / (v1+v2)
+minute = hour * 60
+print(minute)
