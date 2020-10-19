e377481ec2d5b213b5a550ec8d4025c38adf8f2e
j40653263 <huseyinkarakaya@std.iyte.edu.tr>
2020-10-19 14:19:36 +0000

    lab&example

---
+++ lab1/example4.py
@@ -0,0 +1,3 @@
+celcius = float(input("What is the temprature: "))
+fahr = celcius * 1.8 + 32
+print(fahr)
---
+++ lab1/example6.py
@@ -0,0 +1,7 @@
+v1 = 80
+v2 = 70 
+distance = 490
+f_distance = 150
+hour = (distance - f_distance) / (v1+v2)
+minute = hour * 60
+print(minute) 
